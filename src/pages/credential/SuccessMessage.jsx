2acd0446c759c61c9583437d8d9988968ed7a593
Pavithra <pavijoe911@gmail.com>
2025-05-01 19:58:20 +0530

    Update

--- src/pages/credential/SuccessMessage.jsx
+++ src/pages/credential/SuccessMessage.jsx
@@ -1,9 +1,11 @@
 import React from "react";
+import Lottie from "lottie-react";
+import successAnimation from "/images/login/success.json"; // adjust the path based on your folder structure
 
 const SuccessMessage = () => {
   return (
     <div className="flex justify-center items-center min-h-screen bg-white px-4">
-      <div className=" bg-[radial-gradient(circle,#2563eb,#164CA1,_#164CA1)] text-white rounded-[24px] shadow-lg w-full max-w-[320px] sm:max-w-[400px] md:max-w-[500px] lg:max-w-[600px] xl:max-w-[700px] p-6 text-center">
+      <div className="bg-[radial-gradient(circle,#2563eb,#164CA1,_#164CA1)] text-white rounded-[24px] shadow-lg w-full max-w-[320px] sm:max-w-[400px] md:max-w-[500px] lg:max-w-[600px] xl:max-w-[700px] p-6 text-center">
         
         {/* Success Animation */}
         <div className="flex justify-center mb-6">

91e101e9d4cb89dbe8b8a293c468b0ca6131384b
marisudhir <marisudhirinklidox@gmail.com>
2025-05-10 10:01:46 +0530

    Update signup_res.jsx

--- src/pages/credential/signup_res.jsx
+++ src/pages/credential/signup_res.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
-import SignupRe from "@/components/signup_reseller";
+// import SignupRe from "@/components/signup_reseller";
+import LoginPage from "../../Components/signup_reseller";
 
+import SignupRe from ""
 function SignupRes() {
   return (
   <SignupRe />

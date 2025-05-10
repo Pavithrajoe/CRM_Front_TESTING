89522217708874b82e6d79dd5ebcc96b0eda1af9
marisudhir <marisudhirinklidox@gmail.com>
2025-05-10 10:07:47 +0530

    Update calenderpage.jsx

--- src/pages/calenderpage.jsx
+++ src/pages/calenderpage.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ProfileHeader from "../Components/common/ProfileHeader";
-import CalendarView from "../../Components/CalendarView";
+import CalendarView from "../Components/CalendarView";
 import GoogleMeet from "../../Components/GoogleMeet";
 
 const CalendarPage = () => {
